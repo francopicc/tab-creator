1263394c868b95e0a463c6b8b2662a3a9bbbd22f
breakerjs <francopiccirilli51@gmail.com>
2023-05-25 18:46:36 -0300

    Primer version

--- app/page.js
+++ app/page.js
@@ -1,113 +1,45 @@
 import Image from 'next/image'
+import Script from 'next/script'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <p className="mt-4 text-stone-300 text-center font-bold text-xs">Creado por Franco Piccirilli</p>
+      <div id="notification" className="hidden fixed top-0 right-0 m-4 w-80 bg-white rounded-lg shadow-lg">
+        <div className="flex items-center justify-between px-4 py-2">
+            <div className="text-lg font-medium text-gray-800" id="title-not">
+              Guardado correctamente
+            </div>
+          </div>
+          <div className="px-4 text-xs mb-2 py-2" id="desc-not">
+            La tablatura se guardo en tu navegador
+          </div>
         </div>
+      <div>
+        <h1 className="text-center mt-10 text-3xl">🧉</h1>
+        <h1 className="text-center font-extrabold text-3xl mt-2">Creador de Tablaturas</h1>
+        <h3 className="text-center font-bold text-xs text-stone-500">Gratis y simple</h3>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col md:flex-row items-center justify-center mt-8 space-x-3 w-full max-w-screen-xs mr-5">
+        <button id="tab-new" className="rounded-md bg-amber-300 ml-3 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-amber-600 transition ease-in-out duration-300 min-w-[200px] mb-3">
+          <i className="fa-solid fa-trash"></i>
+          <span className="ml-2" id="reset-tab">Reiniciar tablatura</span>
+        </button>
+        <button id="tab-add" className="rounded-md bg-amber-300 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-amber-600 transition ease-in-out duration-300 min-w-[200px] mb-3">
+          <i className="fa-solid fa-square-plus"></i>
+          <span className="ml-2">Crear columna</span>
+        </button>
+        <button id="tab-copy" className="rounded-md bg-amber-300 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-amber-600 transition ease-in-out duration-300 min-w-[200px] mb-3">
+          <i className="fa-solid fa-copy"></i>
+          <span className="ml-2">Copiar tablatura</span>
+        </button>
+        {/* <button id="tab-save" className="rounded-md bg-amber-300 px-3 py-2 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-amber-600 transition ease-in-out duration-300 min-w-[200px] mb-3">
+          <i className="fa-solid fa-floppy-disk"></i>
+          <span className="ml-2">Exportar a PDF</span>
+        </button> */}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p className="text-center text-stone-400 text-xs mt-4">(/) Slide Up  (\) Slide Down  (h) Hammer On  (p) Pull Off  (b) Bend (r) Release (v) Vibrato</p>
+      <Script src="javascript/tab.js" />
     </main>
   )
 }
---
+++ public/javascript/tab.js
@@ -0,0 +1,191 @@
+const tablature_create = async () => {
+    // Restringir caracteres
+    function restrictInput(event) {
+        const allowedChars = /^[0-9\/\\hprvb]$/; // Expresión regular para los caracteres permitidos
+        const inputValue = event.target.value;
+        const lastChar = inputValue[inputValue.length - 1];
+        if (!allowedChars.test(lastChar)) {
+            event.target.value = inputValue.slice(0, -1); // Eliminar el último carácter no permitido
+        }
+    }
+
+    // Configuracion de la notificacion
+    const notification = document.getElementById('notification');
+    const titlenot = document.getElementById("title-not");
+    const descnot = document.getElementById("desc-not");
+
+    // Crea la tabla y agrega las 6 filas iniciales
+    const tabEditor = document.createElement("div");
+    tabEditor.id = "tab-editor";
+    let tabla = document.createElement("table");
+    tabla.id = "tab";
+    for (let i = 0; i < 6; i++) {
+        let fila = document.createElement("tr");
+        for (let j = 0; j < 6; j++) {
+            let celda = document.createElement("td");
+            let input = document.createElement("input");
+            input.type = "text";
+            input.min = 0;
+            input.value = "";
+            input.id = "tab-input";
+            input.placeholder = "-";
+            input.maxLength = 2; // Máximo de 2 caracteres
+            input.addEventListener("input", restrictInput); // Agregar el evento de restricción de caracteres
+            celda.appendChild(input);
+            fila.appendChild(celda);
+        }
+        tabla.appendChild(fila);
+    }
+
+    // Crea el botón y agrega un event listener para agregar columnas a la tabla
+    let botonAgregar = document.getElementById("tab-add");
+    botonAgregar.addEventListener("click", () => {
+        let filas = tabla.getElementsByTagName("tr");
+        for (let i = 0; i < filas.length; i++) {
+            let celda = document.createElement("td");
+            let input = document.createElement("input");
+            input.type = "text";
+            input.min = 0;
+            input.value = "";
+            input.id = "tab-input";
+            input.placeholder = "-"
+            input.maxLength = 2; // Máximo de 2 caracteres
+            celda.appendChild(input);
+            filas[i].appendChild(celda);
+        }
+    });
+
+    // Crea el botón para generar la tablatura y agregar un event listener
+    let botonTablatura = document.getElementById("tab-copy");
+    botonTablatura.addEventListener("click", async () => {
+        let filas = tabla.getElementsByTagName("tr");
+        let tablatura = "";
+        const notas = ["E", "B", "G", "D", "A", "E"]; // Notas de las cuerdas
+
+        for (let i = 0; i < filas.length; i++) {
+            let celdas = filas[i].getElementsByTagName("td");
+            tablatura += notas[i] + "|"; // Agrega la nota de la cuerda
+
+            for (let j = 0; j < celdas.length; j++) {
+                let valor = celdas[j].getElementsByTagName("input")[0].value;
+                if (valor == "") {
+                    valor = "-";
+                }
+                tablatura += valor + "-";
+            }
+            tablatura = tablatura.slice(0, -1); // Elimina el guion al final de la fila
+            tablatura += "\n"; // Agrega un salto de línea al final de cada fila
+        }
+
+        navigator.clipboard.writeText(tablatura)
+            .then(() => {
+                // Notificación
+                notification.classList.remove('hidden', 'animate__fadeOut');
+                notification.classList.add('animate__fadeIn');
+                titlenot.textContent = "Copiada correctamente al portapapeles.";
+                descnot.textContent = "La tablatura se encuentra en tu portapapeles.";
+                setTimeout(() => {
+                    notification.classList.add('animate__fadeOut');
+                    notification.classList.remove('animate__fadeIn');
+                    setTimeout(() => {
+                        notification.classList.add('hidden');
+                    }, 500);
+                }, 1500);
+            })
+            .catch(err => {
+                // Notificación
+                notification.classList.remove('hidden', 'animate__fadeOut');
+                notification.classList.add('animate__fadeIn');
+                titlenot.textContent = "Ocurrió un error al copiar al portapapeles.";
+                descnot.textContent = "Podría ser un error de tu navegador o del código 🤔";
+                setTimeout(() => {
+                    notification.classList.add('animate__fadeOut');
+                    notification.classList.remove('animate__fadeIn');
+                    setTimeout(() => {
+                        notification.classList.add('hidden');
+                    }, 500);
+                }, 1500);
+            });
+    });
+
+
+
+    // Notificacion
+    // notification.classList.remove('hidden', 'animate__fadeOut');
+    // notification.classList.add('animate__fadeIn');
+    // titlenot.textContent = "Guardado correctamente."
+    // descnot.textContent = "La tablatura se guardo en tu navegador."
+    // setTimeout(() => {
+    //     notification.classList.add('animate__fadeOut');
+    //     notification.classList.remove('animate__fadeIn');
+    //     setTimeout(() => {
+    //         notification.classList.add('hidden');
+    //     }, 500)
+    // }, 1500); // 2500 milisegundos = 2.5 segundos
+
+    let number = Math.floor(Math.random() * 6)
+    if (number == 1) {
+        // Notificacion
+        notification.classList.remove('hidden', 'animate__fadeOut');
+        notification.classList.add('animate__fadeIn');
+        titlenot.textContent = "Continua el trabajo donde lo dejaste."
+        descnot.textContent = "Puedes borrar tu tablatura o tambien seguirla."
+        setTimeout(() => {
+            notification.classList.add('animate__fadeOut');
+            notification.classList.remove('animate__fadeIn');
+            setTimeout(() => {
+                notification.classList.add('hidden');
+            }, 500)
+        }, 1500);
+    }
+    let btntouch = false;
+    document.getElementById("tab-new").addEventListener("click", () => {
+        if (!(btntouch)) {
+            setTimeout(() => {
+                btntouch = true;
+            }, 300)
+            document.getElementById("tab-new").style.backgroundColor = "rgb(185 28 28)"
+            document.getElementById("reset-tab").textContent = "Estas seguro?"
+            setTimeout(() => {
+                btntouch = false
+                document.getElementById("tab-new").style.backgroundColor = "rgb(252 211 77)"
+                document.getElementById("reset-tab").textContent = "Reiniciar tablatura"
+            }, 5000)
+        }
+        if (btntouch) {
+            let filas = tabla.getElementsByTagName("tr");
+            for (let i = 0; i < filas.length; i++) {
+                let celdas = filas[i].getElementsByTagName("td");
+                for (let j = 0; j < celdas.length; j++) {
+                    let valor = "-";
+                    celdas[j].getElementsByTagName("input")[0].value = valor;
+                }
+            }
+            localStorage.getItem("tab") == "";
+            // Notificacion
+            document.getElementById("tab-new").style.backgroundColor = "rgb(252 211 77)"
+            document.getElementById("reset-tab").textContent = "Reiniciar tablatura"
+            notification.classList.remove('hidden', 'animate__fadeOut');
+            notification.classList.add('animate__fadeIn');
+            titlenot.textContent = "La tablatura ha sido borrada correctamente."
+            descnot.textContent = "Borraste definitivamente tu tablatura, no es recuperable."
+            setTimeout(() => {
+                notification.classList.add('animate__fadeOut');
+                notification.classList.remove('animate__fadeIn');
+                setTimeout(() => {
+                    notification.classList.add('hidden');
+                }, 500)
+            }, 1500); // 2500 milisegundos = 2.5 segundos
+            setTimeout(() => {
+                btntouch = false
+            }, 200)
+        }
+    })
+    // Agrega la tabla y los botones al elemento tabEditor
+    tabEditor.appendChild(tabla);
+
+    // Agrega el elemento tabEditor al cuerpo del documento
+    document.body.appendChild(tabEditor);
+}
+
+tablature_create();
